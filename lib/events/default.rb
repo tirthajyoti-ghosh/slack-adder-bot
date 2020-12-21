1fb534493887ed31b9678a6d950c24df290bea0a
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-12-22 02:22:55 +0530

    Abstract logic from event handling

--- lib/events/default.rb
+++ lib/events/default.rb
@@ -5,20 +5,32 @@ SlackRubyBotServer::Events.configure do |config|
     slack_client = Slack::Web::Client.new(token: ENV['SLACK_BOT_USER_ACCESS_TOKEN'])
     
     if !event[:event][:bot_id]
-      if event[:event][:text] =~ /^\d+\ ?\+\ ?\d+$/
-
-        msg = event[:event][:text]
-        
-        num1, num2 = msg.split('+')
-        result = num1.to_i + num2.to_i
-        
-        bot_msg = "#{msg} = #{result}"
-        slack_client.chat_postMessage(channel: event[:event][:channel], text: "#{bot_msg}")
-      else
-        slack_client.chat_postMessage(channel: event[:event][:channel], text: "Sorry, I didn't understand that. I only add numbers in this format. eg: 5+6 or 6+3")
-      end
+      slack_client.chat_postMessage(channel: event[:event][:channel], text: BotUser.new.say(event[:event][:text]))
     end
 
     { ok: true }
+  end
+end
+
+class BotUser
+  def say(msg)
+    if msg_valid?(msg)
+      add_from(msg)
+    else
+      "Sorry, I didn't understand that. I only add numbers in this format. eg: 5+6 or 6+3"
+    end
+  end
+
+  private
+
+  def msg_valid?(msg)
+    msg =~ /^\d+\ ?\+\ ?\d+$/
+  end
+
+  def add_from(msg)
+    num1, num2 = msg.split('+')
+    result = num1.to_i + num2.to_i
+    
+    "#{msg} = #{result}"
   end
 end
